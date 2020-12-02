4571c17c4fe8353e7907f4ed040258139071547f
BrainVirus <brandon.fahnestock@gmail.com>
2020-12-02 10:40:23 -0700

    Create 1.a.ps1

---
+++ 2020/1.a.ps1
@@ -0,0 +1,14 @@
+﻿$numbers = Get-Content C:\admin\testtext.txt
+
+foreach ($number1 in $numbers){
+
+  foreach ($number2 in $numbers){
+    $sum = [int]$number1 + [int]$number2
+    if ($sum -eq 2020){
+     $answer =  [int]$number1 * [int]$number2
+      "$number1 + $number2 = $sum"
+      "Answer is $answer"
+      break
+    }
+  }
+}
