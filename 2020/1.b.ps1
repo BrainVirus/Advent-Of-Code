0980b6a21fff07339846690f1e295cddde397ced
BrainVirus <brandon.fahnestock@gmail.com>
2020-12-02 10:40:26 -0700

    Create 1.b.ps1

---
+++ 2020/1.b.ps1
@@ -0,0 +1,18 @@
+﻿$numbers = Get-Content C:\admin\testtext.txt
+$solved = $false
+foreach ($number1 in $numbers){
+if($solved){break}
+  foreach ($number2 in $numbers){
+  if($solved){break}
+    foreach ($number3 in $numbers){
+    if($solved){break}
+   
+    $sum = [int]$number1 + [int]$number2 + [int]$number3
+    if ($sum -eq 2020){
+      $answer =  [int]$number1 * [int]$number2 * [int]$number3
+      "Answer is $answer"
+      $solved = $true
+      }
+    }
+  }
+}
