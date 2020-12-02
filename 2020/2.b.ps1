e0ceeeddc099bfe89bbcc1b95b32cf3f952b4922
BrainVirus <brandon.fahnestock@gmail.com>
2020-12-02 10:40:31 -0700

    Create 2.b.ps1

---
+++ 2020/2.b.ps1
@@ -0,0 +1,18 @@
+﻿$data = Get-Content C:\admin\testtext2.txt
+
+$correctPWCount = 0
+
+foreach ($pwData in $data){
+
+  $pwData = $pwData -split '-| |:'
+  $charpos1 = $pwData[0]-1
+  $charpos2 = $pwData[1]-1
+  $testChar = $pwData[2]
+  $charCount =$pwData[4].ToCharArray()  
+  
+  if (($charCount[$charpos1] -eq $testChar) -xor ($charCount[$charpos2] -eq $testChar)){
+     $correctPWCount++
+  }
+} 
+
+"There are $correctPWCount correct passwords"
