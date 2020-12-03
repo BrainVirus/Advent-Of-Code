5f796f18aeccae9e33a7fe5c0f2a6319cce5a9e9
BrainVirus <brandon.fahnestock@gmail.com>
2020-12-02 23:57:10 -0700

    Create 3.a.ps1

---
+++ 2020/3.a.ps1
@@ -0,0 +1,21 @@
+﻿$map = Get-Content C:\admin\map.txt
+
+$right = 3
+$down = 1
+$treeCounter = 0
+$location = 0
+
+foreach ($line in $map){
+  if ($down -gt 1){
+    $location = $location + $right
+    if ($location -ge $line.Length){
+      $location = $location - $line.Length 
+    }
+    $lineArray = $line.ToCharArray()
+    if ($lineArray[$location] -eq '#'){
+      $treeCounter++
+    }
+  }
+  $down++
+ }
+ "count = $treeCounter"
