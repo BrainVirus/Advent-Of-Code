085213188c4f93a78237122a87d7bb3a54225c6b
BrainVirus <brandon.fahnestock@gmail.com>
2020-12-02 23:57:13 -0700

    Create 3.b.ps1

---
+++ 2020/3.b.ps1
@@ -0,0 +1,57 @@
+﻿$map = Get-Content C:\admin\map.txt
+
+$rightArray = 1,3,5,7
+#$rightArray = 3
+$treeGrandTotal = @()
+
+foreach ($right in $rightArray){
+  $treeCounter = 0
+  $location = 0
+  $down = 1
+  foreach ($line in $map){
+    if ($down -gt 1){
+      $location = $location + $right
+      if ($location -ge $line.Length){
+        $location = $location - $line.Length 
+      }
+      $lineArray = $line.ToCharArray()
+      if ($lineArray[$location] -eq '#'){
+        $treeCounter++
+      }
+    }
+    $down++
+  }
+  "count = $treeCounter"
+  $treeGrandTotal += $treeCounter
+ }
+ 
+ $rightArray = 1
+ 
+ foreach ($right in $rightArray){
+  $treeCounter = 0
+  $location = 0
+  $down = 0
+  foreach ($line in $map){
+    if (($down -ge 2) -and ($down % 2-eq 0)){
+      $location = $location + $right
+      if ($location -ge $line.Length){
+        $location = $location - $line.Length 
+      }
+      $lineArray = $line.ToCharArray()
+      if ($lineArray[$location] -eq '#'){
+        $treeCounter++
+        "tree on line $down"
+      }
+    }
+    $down++
+  }
+  "count = $treeCounter"
+  $treeGrandTotal += $treeCounter
+ }
+ 
+$Command=$treeGrandTotal -join "*"
+$Result = Invoke-Expression -Command $Command
+Write-Output $Result
+"Grand Total Trees = $Result "
+ 
+
